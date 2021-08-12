88d0f1fa792d2dd9c0f74547d6cb99d3dd915737
Juan Chacon <mahnimex@gmail.com>
2021-08-12 15:48:41 -0400

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,42 +17,42 @@ export class AppComponent {
       image: 'assets/images/camiseta.png',
       title: 'Camiseta',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
     {
       id: '2',
       image: 'assets/images/hoodie.png',
       title: 'Hoodie',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
     {
       id: '3',
       image: 'assets/images/mug.png',
       title: 'Mug',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
     {
       id: '4',
       image: 'assets/images/pin.png',
       title: 'Pin',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
     {
       id: '5',
       image: 'assets/images/stickers1.png',
       title: 'Stickers',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
     {
       id: '6',
       image: 'assets/images/stickers2.png',
       title: 'Stickers',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
   ];
 
@@ -62,5 +62,10 @@ export class AppComponent {
 
   deleteItem(index: number) {
     this.items.splice(index, 1);
+  }
+
+  clickProduct(id: number) {
+    console.log('product');
+    console.log(id);
   }
 }

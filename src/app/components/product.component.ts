66c230feeef492e9f2c6cddf5cb326de179d0072
Juan Chacon <mahnimex@gmail.com>
2021-08-14 18:15:45 -0400

    routes

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -14,12 +14,14 @@ import { Product } from '../product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-	styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product!: Product;
 
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  today = new Date();
 
   constructor() {
     console.log('1. constructor');

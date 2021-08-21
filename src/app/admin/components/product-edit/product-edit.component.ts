8b0dcf9506440a5ef76f55fed4c10f7af5456939
Juan Chacon <juan@stracos.com>
2021-08-21 18:24:33 -0400

    edit form

--- src/app/admin/components/product-edit/product-edit.component.ts
+++ src/app/admin/components/product-edit/product-edit.component.ts
@@ -1,34 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ProductsService } from 'src/app/core/services/products/products.service';
-import { MyValidators } from '../../../utils/validators'
+import { Product } from 'src/app/product.model';
+import { MyValidators } from '../../../utils/validators';
 
 @Component({
-  selector: 'app-form-product',
-  templateUrl: './form-product.component.html',
-  styleUrls: ['./form-product.component.scss'],
+  selector: 'app-product-edit',
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.scss'],
 })
-export class FormProductComponent implements OnInit {
+export class ProductEditComponent implements OnInit {
   form!: FormGroup;
+  id!: string;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private activeRoute: ActivatedRoute
   ) {
     this.buildForm();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe((params: Params) => {
+      this.id = params.id;
+      this.productsService.getProduct(this.id).subscribe((product: Product) => {
+        this.form.patchValue(product);
+      });
+    });
+  }
 
   saveProduct(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
       const product = this.form.value;
-      this.productsService.createProduct(product).subscribe((newProduct) => {
+      this.productsService.updateProduct(this.id, product).subscribe((newProduct) => {
         console.log(newProduct);
-        this.router.navigate(['/admin/products'])
+        this.router.navigate(['/admin/products']);
       });
     }
     console.log(this.form.value);
@@ -36,7 +46,6 @@ export class FormProductComponent implements OnInit {
 
   private buildForm() {
     this.form = this.formBuilder.group({
-      id: ['', [Validators.required]],
       title: ['', [Validators.required]],
       price: [0, [Validators.required, MyValidators.isPriceValid]],
       image: [''],
